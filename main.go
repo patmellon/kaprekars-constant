3ac1ef31bf4e5d9bc36dce875c248b3eba9e5c18
Pat Mellon <patmellon@github.com>
2022-08-29 14:37:03 -0400

    Print sorted digits

--- main.go
+++ main.go
@@ -28,5 +28,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(digitString)
+	splitString := strings.Split(digitString, "")
+
+	sortAscend := copyDigitSlice(splitString)
+	sortDescend := copyDigitSlice(splitString)
+
+	// Sorts ascending
+	sort.Strings(sortAscend)
+
+	// Sorts descending
+	sort.Slice(sortDescend, func(i, j int) bool {
+		return sortDescend[i] > sortDescend[j]
+	})
+
+	largerInt := convertToInt(sortDescend)
+	smallerInt := convertToInt(sortAscend)
+
+	fmt.Println(splitString)
+	fmt.Println(largerInt)
+	fmt.Println(smallerInt)
+}
+
+func convertToInt(digit []string) int {
+	intVar, err := strconv.Atoi(strings.Join(digit, ""))
+
+	if err != nil {
+		fmt.Println(nil)
+		os.Exit(1)
+	}
+
+	return intVar
+}
+
+func copyDigitSlice(splitString []string) []string {
+	return append([]string{}, splitString...)
 }
