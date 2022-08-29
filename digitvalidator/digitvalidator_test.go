a0b13ddc64de9ba0cb8377a397f08412d0fa4345
Pat Mellon <patmellon@github.com>
2022-08-29 13:32:30 -0400

    Update tests

--- digitvalidator/digitvalidator_test.go
+++ digitvalidator/digitvalidator_test.go
@@ -8,7 +8,7 @@ import (
 func TestValidateWithNoError(t *testing.T) {
 
 	digitString := "1234"
-	result := ValidateCharacterLength(digitString)
+	result := validateCharacterLength(digitString)
 
 	if result != nil {
 		t.Errorf("Result was incorrect, got: %d, want: nil", result)
@@ -16,18 +16,63 @@ func TestValidateWithNoError(t *testing.T) {
 
 }
 
-func TestValidateWithError(t *testing.T) {
+func TestValidateLengthError(t *testing.T) {
 
 	tests := []string{"123", "45678"}
 
 	expectedError := errors.New("Digit must have a length of 4")
 
 	for _, e := range tests {
-		result := ValidateCharacterLength(e)
+		result := validateCharacterLength(e)
 
 		if result.Error() != expectedError.Error() {
 			t.Errorf("Result was incorrect, got: %d, want: nil", result)
 		}
 	}
 
+}
+
+func TestValidateIntError(t *testing.T) {
+
+	digitString := "word"
+	expectedError := errors.New("Argument is not a number")
+
+	result := validateDigitIsInt(digitString)
+
+	if result.Error() != expectedError.Error() {
+		t.Errorf("got %q, expected %q", result, expectedError)
+	}
+
+}
+
+func TestValidateWithNoErrors(t *testing.T) {
+	digitString := "1234"
+
+	result := Validate(digitString)
+
+	if result != nil {
+		t.Errorf("Result was incorrect, got: %d, want: nil", result)
+	}
+}
+
+func TestValidateWithCharErr(t *testing.T) {
+	digitString := "123"
+	expectedError := errors.New("Digit must have a length of 4")
+
+	result := Validate(digitString)
+
+	if result.Error() != expectedError.Error() {
+		t.Errorf("Result was incorrect, got: %d, want: nil", result)
+	}
+}
+
+func TestValidateWithIntErr(t *testing.T) {
+	digitString := "word"
+	expectedError := errors.New("Argument is not a number")
+
+	result := Validate(digitString)
+
+	if result.Error() != expectedError.Error() {
+		t.Errorf("Result was incorrect, got: %d, want: nil", result)
+	}
 }
